7bbeeacb2fa4e7b1e213a66d31edb3a7cf38df0e
IKaRos <w71800@gmail.com>
2026-05-06 14:30:33 +0800

    chore: 安裝 tailwind typograph plugin 並整合

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,26 @@
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
   darkMode: 'media',
   theme: {
-    extend: {},
+    extend: {
+      typography: ({ theme }) => ({
+        DEFAULT: {
+          css: {
+            '--tw-prose-headings': 'white', // 所有標題顏色
+            '--tw-prose-body': '#CCCCCC',
+            h2: {
+              color: 'var(--tw-prose-headings)',
+              fontWeight: '700',
+              marginTop: '2em',
+              marginBottom: '0.8em',
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [typography],
 };
